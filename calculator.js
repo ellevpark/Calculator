963de50dd1fed70f92bda39bc6d8dd33f690e3af
Ellen Park <ellenpark781@gmail.com>
2021-01-22 14:04:51 -0500

    created post method to calculate sum

--- calculator.js
+++ calculator.js
@@ -1,12 +1,26 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express(); 
+
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.listen(3000, function() {
   console.log("I am listening on port 3000")
 });
 
 app.get("/", function(req, res) {
-  res.send("Hello World")
+  res.sendFile(__dirname + "/index.html")
+});
+
+app.post("/", function(req, res) {
+  let num1 = Number(req.body.n1); 
+  let num2 = Number(req.body.n2);
+
+  let sum = num1 + num2;
+
+  res.send("The sum is " + sum);
 })
+
+
 
 
